234a58de41c7ec7cc66d96f4687b39ae94f4a7be
Gabriel <GABEHJ2001@GMAIL.COM>
2022-01-27 12:18:22 -0800

    JS done

--- JS/todo.js
+++ JS/todo.js
@@ -1,8 +1,45 @@
-var text = {'feeling':'Good', 'time':'Morning'}
-var text1 = {'greeting':'Hello', 'afterG':'There!'}
+function getTodos() {
+    //Array of tasks
+    var todos = new Array;
+    //pull task
+    var todosStr = localStorage.getItem('todo');
+    //if input is not null, JSON.parse will send data to the browser
+    //to make a new task
+    if (todosStr !== null) {
+        todos = JSON.parse(todosStr);
+    }
+    return todos;
+}
 
-//var jsn = JSON.stringify(text);
-var Jstr = JSON.parse(text1);
+//this'll add the inputed task to the getTodos function
+function add() {
+    //this takes the inputed task and creates a variable
+    var task = document.getElementById('task').value;
 
-document.getElementById('text1').innerHTML = jsn;
-document.getElementById('text2').innerHTML = Jstr.greeting + '' + Jstr.afterG;
+    var todos = getTodos();
+    //this'll add a new task at the end of the array
+    todos.push(task);
+    //converts task input to JSON string
+    localStorage.setItem('todo', JSON.stringify(todos));
+    document.getElementById('task').value = '';
+    show();
+
+    return false;
+}
+
+//keeps task displayed in the screen
+function show() {
+    var todos = getTodos();
+
+    //this sets up each task as an unordered list
+    var html = '<ul>';
+    for (var i = 0; todos.length; i++){
+        html += '<li>' = todos[i] + '<button class="remove" id="' + i + 
+            '">x</button></li>';
+    }
+    html += '</ul>';
+    document.getElementById('todos').innerHTML = html;
+}
+
+document.getElementById('add').addEventListener('click', add);
+show();
